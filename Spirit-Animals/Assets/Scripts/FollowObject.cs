095d22fbd100385393a9c0266ad32854de957a91
Tomas Durkin <tdurkin1@gmail.com>
2020-12-22 18:24:29 -0800

    Upload Existing Project

---
+++ Spirit-Animals/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [Range(0.01f, 1.0f)]
+    [SerializeField] float smooth = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, smooth);
+    }
+}
---
+++ Spirit-Animals/Assets/Scripts/SpiritAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiritAI : MonoBehaviour
+{
+    [Tooltip("Navmesh Agent Target. Set to Spirit Target")]
+    [SerializeField] Transform spiritTarget;
+
+    [Tooltip("Turning Speed. Default is 4")]
+    [SerializeField] float turnSpeed = 4f;
+
+    private Camera mainCamera;
+
+    NavMeshAgent navMeshAgent;
+    Animator animController;
+    float distToTarget = Mathf.Infinity;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        distToTarget = Vector3.Distance(transform.position, spiritTarget.position);
+        if (distToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+            animController.SetBool("isWalking", true);
+        } else
+        {
+            FaceCamera();
+            animController.SetBool("isWalking", false);
+        }
+    }
+
+    private void FaceCamera()
+    {
+        
+        Vector3 lookDirection = (mainCamera.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+        
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(spiritTarget.position);
+    }
+}
